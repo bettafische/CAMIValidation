ff96ff09558e6391920b49672ae1c5d2d3105052
bettafische <51241276+bettafische@users.noreply.github.com>
2025-07-29 10:27:41 -0700

    delete makeManifest_Plant_longReads_PacBio.sh
    deleted from main; placed in makeSketchManifest directory.

--- makeManifest_Plant_longReads_PacBio.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-echo name,genome_filename,protein_filename > /group/ctbrowngrp4/2025-olivas-chillFilter/data/CAMI_plant/CAMI_Plant_longReads_PacBio_Sketch.csv
-for i in /group/ctbrowngrp4/2025-olivas-chillFilter/data/CAMI_plant/sampleDownload_longReads_PacBio/simulation_pbsim/2019.11.04_11.02.31_sample_*/reads/anonymous_reads.fq.gz
-do
-  sample=$(basename "$(dirname "$(dirname "$i")")" | sed 's/.*sample_//')
-  echo CAMIPlant_longReads_PacBio_$sample,$i,
-done >> /group/ctbrowngrp4/2025-olivas-chillFilter/data/CAMI_plant/CAMI_Plant_longReads_PacBio_Sketch.csv
