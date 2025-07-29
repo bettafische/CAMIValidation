c662aa7fc128864a79cc3d91fd50ba856410c3e9
bettafische <51241276+bettafische@users.noreply.github.com>
2025-07-29 10:27:11 -0700

    delete makeManifest_Marine_longReads.sh
    deleted from main; placed in makeSketchManifest directory.

--- makeManifest_Marine_longReads.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-echo name,genome_filename,protein_filename > /group/ctbrowngrp4/2025-olivas-chillFilter/data/CAMI_marine/CAMI_Plant_longReads_Sketch.csv
-for i in /group/ctbrowngrp4/2025-olivas-chillFilter/data/CAMI_marine/sampleDownload_longReads/simulation_long_read/2018.08.08_16.35.17_sample_*/reads/anonymous_reads.fq.gz
-do
-  sample=$(basename "$(dirname "$(dirname "$i")")" | sed 's/.*sample_//')
-  echo CAMIMarine_longReads_$sample,$i,
-done >> /group/ctbrowngrp4/2025-olivas-chillFilter/data/CAMI_marine/CAMI_Plant_longReads_Sketch.csv
