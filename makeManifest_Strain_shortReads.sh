91734eca3e7397f19d5866a2bb3d1028749ae50c
bettafische <51241276+bettafische@users.noreply.github.com>
2025-07-29 10:28:10 -0700

    delete makeManifest_Strain_shortReads.sh
    deleted from main; placed in makeSketchManifest directory.

--- makeManifest_Strain_shortReads.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-echo name,genome_filename,protein_filename > /group/ctbrowngrp4/2025-olivas-chillFilter/data/CAMI_strainMadness/CAMI_StrainMadness_shortReads_Sketch.csv
-for i in /group/ctbrowngrp4/2025-olivas-chillFilter/data/CAMI_strainMadness/sampleDownload_shortReads/short_read/2018.09.07_11.43.52_sample_*/reads/anonymous_reads.fq.gz
-do
-  sample=$(basename "$(dirname "$(dirname "$i")")" | sed 's/.*sample_//')
-  echo CAMIStrain_shortReads_$sample,$i,
-done >> /group/ctbrowngrp4/2025-olivas-chillFilter/data/CAMI_strainMadness/CAMI_StrainMadness_shortReads_Sketch.csv
