5c5073c1accd088e6c9e3c4d785a2218c7c04af4
bettafische <51241276+bettafische@users.noreply.github.com>
2025-07-30 13:54:49 -0700

    Add files via upload
    Wrong directory call.

--- makeSketchManifest/makeManifest_Marine_longReads.sh
+++ makeSketchManifest/makeManifest_Marine_longReads.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-echo name,genome_filename,protein_filename > /group/ctbrowngrp4/2025-olivas-chillFilter/data/CAMI_marine/CAMI_Plant_longReads_Sketch.csv
+echo name,genome_filename,protein_filename > /group/ctbrowngrp4/2025-olivas-chillFilter/data/CAMI_marine/CAMI_Marine_longReads_Sketch.csv
 for i in /group/ctbrowngrp4/2025-olivas-chillFilter/data/CAMI_marine/sampleDownload_longReads/simulation_long_read/2018.08.08_16.35.17_sample_*/reads/anonymous_reads.fq.gz
 do
   sample=$(basename "$(dirname "$(dirname "$i")")" | sed 's/.*sample_//')
--- makeSketchManifest/makeManifest_Marine_shortReads.sh
+++ makeSketchManifest/makeManifest_Marine_shortReads.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-echo name,genome_filename,protein_filename > /group/ctbrowngrp4/2025-olivas-chillFilter/data/CAMI_marine/CAMI_Plant_shortReads_Sketch.csv
+echo name,genome_filename,protein_filename > /group/ctbrowngrp4/2025-olivas-chillFilter/data/CAMI_marine/CAMI_Marine_shortReads_Sketch.csv
 for i in /group/ctbrowngrp4/2025-olivas-chillFilter/data/CAMI_marine/sampleDownload_shortReads/simulation_short_read/2018.08.15_09.49.32_sample_*/reads/anonymous_reads.fq.gz
 do
   sample=$(basename "$(dirname "$(dirname "$i")")" | sed 's/.*sample_//')
